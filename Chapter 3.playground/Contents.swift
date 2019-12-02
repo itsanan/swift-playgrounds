85027e686b1a8f36f417bf03a5967b86c403fa8e
itsanan <valaddict007@gmail.com>
2019-12-02 07:54:14 +0700

    First chance #3

--- Chapter 3.playground/Contents.swift
+++ Chapter 3.playground/Contents.swift
@@ -68,3 +68,37 @@ if isTeenager {
 */
 
 let answer = isTeenager ? "Teenager" : "Not a Teenager"
+
+// Mini-exercises
+
+/*
+    1. Create a variable named counter and set it equal to 0. Create a while loop with
+    the condition counter < 10 which prints out counter is X (where X is replaced
+    with counter value) and then increments counter by 1.
+*/
+
+var counter = 0
+
+while (counter < 10) {
+    counter += 1
+    print("Counter is : " + String(counter))
+}
+
+counter = 0
+
+/*
+    2. Create a variable named counter and set it equal to 0. Create another variable
+    named roll and set it equal to 0. Create a repeat-while loop. Inside the loop,
+    set roll equal to Int.random(in: 0...5) which means to pick a random
+    number between 0 and 5. Then increment counter by 1. Finally, print After X
+    rolls, roll is Y where X is the value of counter and Y is the value of roll.
+    Set the loop condition such that the loop finishes when the first 0 is rolled.
+*/
+
+var roll = 0
+
+repeat {
+    roll = Int.random(in: 0 ... 5)
+    counter += 1
+    print("After \(counter) rolls, roll is \(roll)")
+} while (roll == 0)
